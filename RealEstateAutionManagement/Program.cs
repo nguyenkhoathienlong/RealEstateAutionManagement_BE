ba269486e4b9562e0e5f7d006a6f29b7df23a6e4
nguyenntse <nguyenntse161785@fpt.edu.vn>
2024-01-16 20:25:43 +0700

    update swagger

--- RealEstateAutionManagement/Program.cs
+++ RealEstateAutionManagement/Program.cs
@@ -49,12 +49,9 @@ builder.Services.AddSwaggerGen(option =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{ c.SwaggerEndpoint("/swagger/v1/swagger.json", "REAS API v1"); });
 
 app.UseCors("AllowAll");
 
