66013e1b5895f985e52c3b1c25a800818b4708ed
Thiên Long <63593749+nguyenkhoathienlong@users.noreply.github.com>
2024-03-25 13:04:24 +0700

    update pageSize

--- Data/Models/QueryStringParameters.cs
+++ Data/Models/QueryStringParameters.cs
@@ -16,7 +16,7 @@ namespace Data.Models
                 _pageIndex = (value > 0) ? value : 1;
             }
         }
-        private int _pageSize = 10;
+        private int _pageSize = 20;
         public int PageSize { get => _pageSize; set => _pageSize = (value > maxPageSize) ? maxPageSize : value; }
         public string? OrderBy { get; set; }
     }
